ef133ac863c56fe3f12b5aa81026178c9ab58b33
Elliot <elliotgriffiss@gmail.com>
2024-02-07 18:54:39 +0000

    Created Index Pages

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as PIXI from 'pixi.js';
+
+/// Global Class To Handle Game Management
+export class PixiAppManager {
+    private m_app: PIXI.Application;
+
+    constructor() {
+        this.m_app = new PIXI.Application({
+            view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+            resolution: window.devicePixelRatio || 1,
+            autoDensity: true,
+            backgroundColor: "blue",
+            width: 1200,
+            height: 800,
+        });
+    };
+}
+
+var TemplateGame = new PixiAppManager();
