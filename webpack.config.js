98ed9da7e5dc1f23e3871db5219c56576335b6d3
Elliot <elliotgriffiss@gmail.com>
2024-02-07 18:51:25 +0000

    Created and setup Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: "inline-source-map",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+        // Add support for TypeScripts fully qualified ESM imports.
+        extensionAlias: {
+            ".js": [".js", ".ts"],
+            ".cjs": [".cjs", ".cts"],
+            ".mjs": [".mjs", ".mts"]
+        }
+    },
+    module: {
+        rules: [
+            // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" }
+        ]
+    },
+    plugins: [
+        // Make an index.html from the template
+        new HtmlWebpackPlugin({
+            template: 'src/index.ejs',
+            hash: true,
+            minify: false
+        })
+    ]
+};
