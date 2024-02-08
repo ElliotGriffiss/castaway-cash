5520386b77b304a1330b4a80eb55301ccdfa0ceb
Elliot <elliotgriffiss@gmail.com>
2024-02-08 18:23:09 +0000

    Made a Game.ts Class to manage project functionality

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import * as PIXI from 'pixi.js';
+import Game from './Game';
 
 /// Global Class To Handle Game Management
 export class PixiAppManager {
-    private m_app: PIXI.Application;
+    private m_game: Game;
 
     constructor() {
-        this.m_app = new PIXI.Application({
+        const app = new PIXI.Application({
             view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
             resolution: window.devicePixelRatio || 1,
             autoDensity: true,
-            backgroundColor: "blue",
+            backgroundColor: "orange",
             width: 1200,
             height: 800,
         });
 
+        this.m_game = new Game();
+
         // @ts-ignore
-        globalThis.__PIXI_APP__ = this.m_app;
+        globalThis.__PIXI_APP__ = app;
     };
 }
 
