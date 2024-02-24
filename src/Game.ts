1e612dc36e31b1630d229f562de123a9614fff10
Elliot <elliotgriffiss@gmail.com>
2024-02-24 10:23:34 +0000

    Adjusted Position so everything is visible on screen

--- src/Game.ts
+++ src/Game.ts
@@ -5,7 +5,10 @@ export class Game {
     constructor() {
 
         const sprite1 = Sprite.from(global.game.Sprite1);
+        sprite1.position = {x: 50, y: 50};
+
         const sprite2 = Sprite.from(global.game['Sprite2']);
+        sprite2.position = {x: 150, y: 50};
 
         const text = new Text('This is a PixiJS text', {
             fontFamily: 'arcadeclassic-webfont',
@@ -13,6 +16,8 @@ export class Game {
             fill: 0xff1010,
             align: 'center',
         });
+
+        text.position = {x: 125, y: 15};
 
         global.app.stage.addChild( sprite1, sprite2, text );
     }
