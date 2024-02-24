5da1bdb73f972f133e22b7abd8326bf4f2e6a93a
Elliot <elliotgriffiss@gmail.com>
2024-02-24 10:16:33 +0000

    Created an Asset Manifest Generator that build a pixi asset manifest from the file structure

---
+++ AssetManifestGenerator.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const fs   = require("fs");
+
+let bundles = [];
+
+function GenerateFromDirectory(Directory) {
+    fs.readdirSync(Directory).forEach(fileName => {
+        const Absolute = path.join(Directory, fileName);
+
+        if (fs.statSync(Absolute).isDirectory()) {
+            let assets = [];
+
+            fs.readdirSync(Absolute).forEach((fileName) => {
+                const fullPath = path.join(Absolute, fileName);
+
+                assets.push( {
+                    name: fileName.split("."[0])[0],
+                    src: fullPath.replace("src\\", "")
+                });
+            });
+
+            bundles.push({
+                name: fileName,
+                assets: assets
+            });
+        }
+    });
+}
+
+GenerateFromDirectory('src/assets');
+
+
+fs.writeFile("src/AssetManifest.json", JSON.stringify( {bundles}
+),function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
