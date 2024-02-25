2527d15880b159b56bf5dead3e703c090b572e8f
Elliot <elliotgriffiss@gmail.com>
2024-02-25 07:28:12 +0000

    Fixed the typing of __PIXI_APP__

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,7 +2,7 @@ import {Application} from "pixi.js";
 
 declare global {
     var app: Application;
-    var __PIXI_APP__: app;
+    var __PIXI_APP__: Application;
     var preload: any;
     var game: any;
 }
